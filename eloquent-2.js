768186f117c44b191f741127cd8079a66dc07f89
hameet-singh <32632890+Hameet@users.noreply.github.com>
2018-01-01 23:47:27 +1300

    Update eloquent-2.js

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,2 +1,12 @@
-for (var number = "#"; number < "#######"; number += "#")
+for (var number = "#"; number < "#######"; number += "#") // I just replicated the given code for a table of 2 and changed it. 
   console.log(number);
+
+//2nd exercise:
+
+for (var number = 0; number <= 100; number++) 
+  { var result = "";
+  if (number % 3 == 0) result = "Fizz";
+  if (number % 5 == 0) result = "Buzz";
+  if (number % 3 == 0 || number % 5 == 0) result = "FizzBuzz"; 
+
+console.log(result || number);}
