b49fd1717548a109bc62ffd37a39e3c77c1a73f8
hameet-singh <32632890+Hameet@users.noreply.github.com>
2018-01-02 13:00:02 +1300

    Update eloquent-2.js

--- eloquent-2.js
+++ eloquent-2.js
@@ -10,3 +10,20 @@ for (var number = 0; number <= 100; number++)
   if (number % 3 == 0 || number % 5 == 0) result = "FizzBuzz"; 
 
 console.log(result || number);}
+
+//chess board Couldn't figure this one out so I had to look the solution up. 
+var size = 8;
+
+var board = "";
+
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0)
+      board += " ";
+    else
+      board += "#";
+  }
+  board += "\n";
+}
+
+console.log(board);
